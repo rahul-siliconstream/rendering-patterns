376064e65b6faf7f6a72ec5f94e29c398c749131
Aman Madhukar <amanmadhukar@MacBook-Air.local>
2026-03-11 13:22:11 +0530

    rendering patterns project initialized

---
+++ src/app/client/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function ClientComp(){
+  return (
+    <div>
+      CLient
+    </div>
+  );
+}
---
+++ src/app/server/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function ServerComp(){
+  return (
+    <div>
+      Server
+    </div>
+  );
+}
