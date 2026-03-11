a008033d5571d37a79f3c18d32adc6d3c51bb9b0
Aman Madhukar <amanmadhukar@MacBook-Air.local>
2026-03-11 13:44:48 +0530

    created POC for csr and ssr

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -1,9 +1,32 @@
+'use client';
 
+import { useEffect, useState } from "react";
+import './style.css';
 
 export default function ClientComp(){
+  const [data, setData] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(res => res.json())
+    .then(res => {
+      setData(res);
+      setLoading(false);
+    });
+  }, []);
+
   return (
-    <div>
-      CLient
+    <div className="container">
+      {
+        loading? <p>Loading...</p>
+        :
+        <div className="card">
+          <p className="title">{data?.title}</p>
+          <p className="para">{data?.body}</p>
+        </div>
+      }
     </div>
   );
 }
--- src/app/server/page.tsx
+++ src/app/server/page.tsx
@@ -1,9 +1,16 @@
+import './style.css';
 
+export default async function ServerComp(){
 
-export default function ServerComp(){
+    const jsonData = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+    const data = await jsonData.json();
+
   return (
-    <div>
-      Server
+    <div className="container">
+      <div className="card">
+        <p className="title">{data?.title}</p>
+        <p className="para">{data?.body}</p>
+      </div>
     </div>
   );
 }
